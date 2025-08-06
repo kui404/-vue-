df37c2d422abf30c720aab8a418faf03f75328ca
kui404 <huakui404@gmail.com>
2025-08-06 13:33:09 +0800

    Initial commit

---
+++ src/components/person.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="person">
+        <h1>你好啊！</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:''
+    }
+</script>
+
+<style>
+    .person {
+        background-color: skyblue;
+        box-shadow: 0 0 10px;
+        border-radius: 10px;
+        padding: 20px;
+    }
+</style>
